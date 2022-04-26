bfb655a5df76c50aac9db046040a1a15e893d0a6
Amanjit313 <amanjitsingh313@gmail.com>
2022-04-26 18:18:21 +0200

    Griglia Campo Minato - Complete

--- js/script.js
+++ js/script.js
@@ -11,27 +11,35 @@ document.getElementById("gioca").addEventListener("click", play);
 function play(){
   reset();
   const difficoltà = document.getElementById("difficoltà").value;
+  console.log(difficoltà);
   const grigliedifficoltà = [100,81,49];
   const numeriCelle = grigliedifficoltà[difficoltà];
   console.log(numeriCelle);
 
-  /* zonagioco(numeriCelle); */
+  zonagioco(numeriCelle);
 }
 
-/* function zonagioco(numeriCelle){
+function zonagioco(numeriCelle){
 
   const contenitore = document.createElement("div");
   contenitore.className = "contenitore";
 
   for(let i = 1; i <= numeriCelle; i++){
+
     const numberBox = document.createElement("div");
     numberBox.className = "number-box cella"+numeriCelle;
-    numberBox.innerHTML = `<span>${i}</span>`
+    numberBox.innerHTML = `<span>${i}</span>`;
     contenitore.append(numberBox);
+
+    numberBox.addEventListener("click", numberBoxClicked);
   }
 
   main.append(contenitore);
-} */
+} 
+
+function numberBoxClicked(){
+  this.classList.add("clicked");
+}
 
 function reset(){
   main.innerHTML = "";
